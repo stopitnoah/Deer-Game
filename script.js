4e2d28c5fa99b06a97b62501483656090e9e8546
Noah Wright <noah.f.wright@gmail.com>
2019-06-06 15:23:06 -0500

    Commit 0.0.1.3.3
    - Fixed bug where Promote Elder cost 0 deer
    
    - Fixed bug where it was possible to purchase a research science multiple times
    
    - Fixed bug where wood limit was not enforced
    
    - Simplified Top-Bar Navigation Button Mechanics
    
    - Simplified Color Coding of Buttons

--- script.js
+++ script.js
@@ -12,22 +12,13 @@ var deercount = 10; //Deer
 var deerincrement = 1; //Birth Increment
 var deermax = 10; //Deer Capacity
 
-var grasslandcount = 0; //Number of Grasslands
-var grasslandprice1 = 15; //Price of Grasslands (Seeds)
-var grasslandprice3 = 1; //Price of Grasslands (Land)
-var grasslandpriceratio1 = 1.5; //Grassland Price Ratio (Seeds)
-var grasslandpriceratio3 = 1.1; //Grassland Price Ratio (Land)
-var grasslandfarmerboost = 0.1; //Farmer Boost from Grasslands
-var grasslandwheatrate = 0.5; //Grassland Wheat Production
-var grasslandseedrate = 0.05; //Grassland Seed Production
-
 var wheatpoint = 0; //Starting Wheat Value
 var wheatmax = 100; //Maximum Wheat Valu
 var wheatrate = 0; //Wheat Generation
 var wheatincrement = 0;
 
 var rockpoint = 0;
-var rockmax = 12.5;
+var rockmax = 12.1;
 var rockrate = 0;
 var rockincrement = 0;
 
@@ -43,22 +34,77 @@ var seedrate = 0; //Seed Generation
 var irrigationproject = false; //Has Irrigation been built?
 var irrigationfertilityboost = 1.20; //Irrigation Fertility Boost
 
-var employabledeer = deercount - jobfarmers - jobscientists - jobminers - jobloggers;
+{
+    
+var availablejobs = deercount - jobfarmers - jobscientists - jobminers - jobloggers;
+
+{
 var jobfarmers = 0; //Farmer Count
-var farmersrate = 0.01 //Farmer Base Generation Rate
-
+var employablefarmers = 0; //Farmer Jobs Available
+var farmerproduction = 0; //Farmer Production Per Tick
+var farmerproductionratio = 1; //Total Improvement Ratio to Farmers
+var farmeroutput = 0; //Final Output of Farmers
+var farmerwheatrate = 0.81; //Farmer Base Generation Rate
+var minerwheatconsumption = 0.08 //Farmer Base Wheat Consumption Rate
+} //Job - Farmer
+{
 var jobminers = 0; //Miners Count
-var minerockrate = 0.25; //Miner Base Generation Rate
+var employableminers = 0; //Miner Jobs Available
+var minerproduction = 0; //Miner Production Per Tick
+var minerproductionratio = 1; //Total Improvement Ratio to Forests
+var mineroutput = 0; //Final Output of Miners
+var minerockrate = 0.025; //Miner Base Generation Rate
+var minerwheatconsumption = 0.21 //Miner Base Wheat Consumption Rate
+} // Job - Miner
 
+{
 var jobloggers = 0; //Loggers Count
-var loggerswoodrate = 0.10; //Logger Base Generation Rate
+var employableloggers = 0; //Logger Jobs Available
+var loggerproduction = 0; //Logger Production Per Tick
+var loggerproductionratio = 1; //Total Improvement Ratio to Loggers
+var loggeroutput = 0; //Final Output of Loggers
+var loggerwoodrate = 0.064; //Logger Base Generation Rate
+var loggerwheatconsumption = 0.23 //Logger Base Wheat Consumption Rate
+} //Job - Logger
 
+} //Jobs Variables
+
+//todo
 var jobscientists = 0; //Scientists Count
-var scientistssciencerate = 0.15; //Scientists Base Generation
+var jobexplorers = 0; //Explorers Count
+var jobmanagers = 0; //Managers Count
 
+{
+var forestcount = 0; //Forest Count
+var forestwoodrate = 0.25; //Forest Base Generation=
+var forestproduction = 0; //Total Output of all Forests
+var forestproductionratio = 1; //Total Improvement Ratio to Forests
+var forestoutput = 0; //Final Output of Forest Buildings
+var forestloggerboost = 0.15; //Forest Logger Boost
+var forestcost1 = 5; //Forest Cost (Seed)
+var forestcost2 = 1; //Forest Cost (Land)
+var forestpriceratio1 = 1.23; //Forest Price Ratio (Seed)
+var forestpriceratio2 = 1.12; //Forest Price Ratio (Land)
+} //Forest Variables
+{
+var grasslandcount = 0; //Number of Grasslands
+var grasslandwheatrate = 0.5; //Grassland Wheat Production
+var grasslandproduction = 0; //Total Output of all Grasslands
+var grasslandproductionratio = 1; //Total Improvement Ratio to Grasslands
+var grasslandoutput = 0; //Final Output of Grassland Buildings
+var grasslandprice1 = 15; //Price of Grasslands (Seeds)
+var grasslandprice3 = 1; //Price of Grasslands (Land)
+var grasslandpriceratio1 = 1.5; //Grassland Price Ratio (Seeds)
+var grasslandpriceratio3 = 1.1; //Grassland Price Ratio (Land)
+var grasslandfarmerboost = 0.1; //Farmer Boost from Grasslands
+var grasslandseedrate = 0.05; //Grassland Seed Production
+} //Grassland Variables
 {
 var minecount = 0; //Mine Count
 var minerockrate = 0.01; //Mine Base Generation
+var mineproduction = 0; //Total Output of all Mines
+var mineproductionratio = 1; //Total Improvement Ratio to Mines
+var mineoutput = 0; //Final Output of Mine Buildings
 var mineminerboost = 0.10; //Mine Miner Boost
 var minecost1 = 10; //Mine Price (Wood)
 var minecost2 = 1;  //Mine Price (Land)
@@ -66,17 +112,10 @@ var minepriceratio1 = 1.28; //Mine Price Ratio (Wood)
 var minepriceratio2 = 1.05; //Mine Price Ratio (Land)
 } //Mine Variables
 
-var forestcount = 0; //Forest Count
-var forestwoodrate = 0.25; //Forest Base Generation
-var forestloggerboost = 0.15; //Forest Logger Boost
-var forestcost1 = 5; //Forest Cost (Seed)
-var forestcost2 = 1; //Forest Cost (Land)
-var forestpriceratio1 = 1.23; //Forest Price Ratio (Seed)
-var forestpriceratio2 = 1.12; //Forest Price Ratio (Land)
+var scrapyardcount = 0; //Field Count
 
-var fieldcount = 0; //Field Count
-
-
+{
+    
 var eldercount = 0; //Elders
 var elderincrement = 1; //ElderIncrement
 var eldermax = 5; //Elder Max
@@ -87,8 +126,11 @@ var eldersciprod = 0.05; //Elder Base Science Production
 var unicorncount = 0; //Unicorns
 var unicornincrement = 0.01;
 var unicornmax = 10000000;
+    
+} //Elder Variables
 
-
+{
+    
 var faithpoint = 0; //Faith Points
 var faithincrement = 1;  //Faith Points per Sacrifice
 var faithmax = 25; //Faith storage
@@ -103,7 +145,10 @@ var landcount = 50;
 var landincrement = 0;
 var landmax = 50;
 var landrate = 0;
+    
+} //Abstract Resource Variables
 
+{
 var minute = "00";
 var hour = 0;
 var AM = true;
@@ -112,10 +157,17 @@ var month = 1;
 var year = 0;
 
 var age = 0;
+} //Time Variables
 
+{
+    
 var aa = false;
 var ab = false;
+    
+} //single-use booleans
 
+{
+    
 var agriculture = false;
 var agriculturecost = 10;
 var mining = false;
@@ -124,24 +176,24 @@ var logging = false;
 var loggingcost = 10;
 var civilservice = false;
 var civilservicecost = 25;
-
-var cardID = [0, "Deer", "Grazing", "The Great White North", deercount]; // cardnum, species, verb, location, count
+    
+} //Science Variables
 
 $(document).ready(function(){
     $("#HerdMgmt").hide();
-    $("#PromoteElder").fadeOut();
+    $("#PromoteElder").hide();
     
     $("#HerdMgmt").click(function(){
         $("#MissionControl").fadeOut(300);
         $(".sciGroupContainer").slideUp(300);
         setTimeout(300);
-        $(".bldGroupContainer").slideDown(300);
+        $(".bldGroupContainer").toggle();
     })    
     $("#ScienceShow").click(function(){
         $(".bldGroupContainer").fadeOut(300);
         $("#MissionControl").fadeOut(300);
         setTimeout(300);
-        $("#sciencePage").slideDown(300);
+        $("#sciencePage").toggle();
     })
     
     $("#SacrificeDeer").mouseover(function(){
@@ -151,18 +203,11 @@ $(document).ready(function(){
         $("#SacrificeDeer2").hide();
     })
     $("#SacrificeDeer").click(function(){    
-        if(cardID[0] == 0 && deercount > 0 && faithpoint < faithmax) {
+        if(deercount > 0 && faithpoint < faithmax) {
             deercount = deercount - 1;
             faithpoint = faithpoint + faithincrement;
             console.log("Sacrificed")
             checkNumbers();
-            if(faithpoint > 4){
-                $("#PromoteDeer").fadeIn();
-            }
-            $("#FaithDiv").slideDown(300);
-            if(deercount < 1){
-                $("#SacrificeDeer").css("background-color", "#A9A9A9");
-            }
             updatePage();
         }
     });
@@ -173,9 +218,10 @@ $(document).ready(function(){
         $("#PromoteDeer2").hide();
     })
     $("#PromoteDeer").click(function(){
-        if(faithpoint >= eldercost && eldercount < eldermax){
+        if(deercount > 1 && faithpoint >= eldercost && eldercount < eldermax){
             eldercount = eldercount + elderincrement; //Get Elder
             faithpoint = faithpoint - eldercost; //Pay Faith
+            deercount = deercount - 1; //Deer Converts to Elder
             $("#ElderDiv").slideDown(300); //Show Elder Resource
             $("#ScienceDiv").slideDown(300); //Show Science Resource
             $("#log").prepend("<br> You have promoted an Elder");
@@ -199,7 +245,8 @@ $(document).ready(function(){
     })
     $("#Forest").mouseover(function(){
         $("#Forest2").show();
-    }).mouseout(function(){
+    })
+    $("#Forest").mouseout(function(){
         $("#Forest2").hide();
     })
     $("#Forest").click(function(){
@@ -226,7 +273,7 @@ $(document).ready(function(){
     })
     
     $("#AgricultureSci").click(function(){
-        if(sciencepoint >= agriculturecost){
+        if(sciencepoint >= agriculturecost && agriculture = false){
             agriculture = true;
             sciencepoint = sciencepoint - agriculturecost;
             AgricultureRect.style.fill='#565656';
@@ -239,7 +286,7 @@ $(document).ready(function(){
         }
     })
     $("#LoggingSci").click(function(){
-        if(sciencepoint >= loggingcost){
+        if(sciencepoint >= loggingcost && logging = false){
             logging = true;
             sciencepoint = sciencepoint - loggingcost;
             LoggingRect.style.fill='#565656';
@@ -252,7 +299,7 @@ $(document).ready(function(){
         }
     })
     $("#MiningSci").click(function(){
-        if(sciencepoint >= miningcost){
+        if(sciencepoint >= miningcost && mining = false){
             mining = true;
             sciencepoint = sciencepoint - miningcost;
             MiningRect.style.fill='#565656';
@@ -288,12 +335,33 @@ function checkNumbers(){
     if(rockpoint > rockmax){
         rockpoint = rockmax;
     }
+    if(woodpoint > woodmax){
+        woodpoint = woodmax;
+    }
     grasslandprice1 = 10 * Math.pow(grasslandpriceratio1, grasslandcount);
     grasslandprice3 = 10 * Math.pow(grasslandpriceratio3, grasslandcount);
     forestcost1 = 10 * Math.pow(forestpriceratio2, forestcount);
     forestcost2 = 10 * Math.pow(forestpriceratio2, forestcount);
     minecost1 = 10 * Math.pow(minepriceratio1, minecount);
     minecost2 = 10 * Math.pow(minepriceratio2, minecount);
+    
+    forestproduction = forestcount * forestwoodrate;
+    forestoutput = forestproduction * forestproductionratio;
+    grasslandproduction = grasslandcount * grasslandwheatrate;
+    grasslandoutput = grasslandproduction * grasslandproductionratio;
+    mineproduction = minecount * minerockrate;
+    mineoutput = mineproduction * mineproductionratio;
+    
+    employableminers = minecount - jobminers;
+    minerproduction = jobminers * minerockrate;
+    mineroutput = minerproduction * minerproductionratio;
+    employablefarmers = grasslandcount - jobfarmers;
+    farmerproduction = jobfarmers * farmerwheatrate;
+    farmeroutput = farmerproduction * farmerproductionratio;
+    employableloggers = forestcount - jobloggers;
+    loggerproduction = jobloggers * loggerwoodrate;
+    loggeroutput = loggerproduction * loggerproductionratio;
+    
 }
 
 function updatePage() {
@@ -324,6 +392,19 @@ function updatePage() {
     } //Ticker Data Active Styling
     
     {
+        
+    $(".jobminers").text(jobminers.toFixed(0));
+    $(".employableminers").text(employableminers.toFixed(0));
+    $(".minerproduction").text(minerproduction.toFixed(2));
+        
+    $(".jobfarmers").text(jobfarmers.toFixed(0));
+    $(".employablefarmers").text(employablefarmers.toFixed(0));
+    $(".farmerproduction").text(farmerproduction.toFixed(2));
+        
+    $(".jobloggers").text(jobloggers.toFixed(0));
+    $(".employableloggers").text(employableloggers.toFixed(0));
+    $(".loggerproduction").text(loggerproduction.toFixed(2));
+        
     $(".Faithpoints").text(faithpoint.toFixed(2));
     $(".FaithMax").text("/" + faithmax.toFixed(0));
     $(".FaithRate").text("(+" + faithrate.toFixed(2) + "/tick)");
@@ -388,11 +469,17 @@ function updatePage() {
     if(deercount >= 1){
         $("#SacrificeDeer").css("background-color", "palegoldenrod");
     }
-    if(eldercost <= faithpoint){
+    if(deercount > 1 && faithpoint >= eldercost && eldercount < eldermax){
         $("#PromoteDeer").css("background-color", "palegoldenrod");
     }
-    if(eldercost > faithpoint){
+    if(deercount < 1 || faithpoint <= eldercost || eldercount >= eldermax){
         $("#PromoteDeer").css("background-color", "#A9A9A9");
+    }
+    if(deercount >= 1 && faithpoint < faithmax){
+        $("#SacrificeDeer").css("background-color", "palegoldenrod");
+    }
+    if(deercount < 1 || faithpoint >= faithmax){
+        $("#SacrificeDeer").css("background-color", "#A9A9A9");
     }
     if(seedpoint >= grasslandprice1 && landcount >= grasslandprice3){
         $("#Grassland").css("background-color", "palegoldenrod");
@@ -484,16 +571,16 @@ setInterval(function gameMath(){
     faithrate = 0;
     faithpoint = faithpoint + faithrate;
     
-    sciencerate = (eldercount * eldersciprod);
+    sciencerate = eldercount * eldersciprod;
     sciencepoint = sciencepoint + sciencerate;
     
-    wheatrate = (grasslandcount * grasslandwheatrate);
+    wheatrate = grasslandoutput + farmeroutput;
     wheatpoint = wheatpoint + wheatrate;
     
-    woodrate = (forestcount * forestwoodrate);
+    woodrate = forestoutput + loggeroutput;
     woodpoint = woodpoint + woodrate;
     
-    rockrate = (minecount * minerockrate);
+    rockrate = mineoutput + mineroutput;
     rockpoint = rockpoint + rockrate;
     
     seedrate = grasslandcount * grasslandseedrate;
